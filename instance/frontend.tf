0820ff19521d0c33441beaf582cabcfd9bd26123
sarithadevops23@gmail.com <sarithadevops23@gmail.com>
2023-06-17 17:56:22 +0530

    ds

--- instance/frontend.tf
+++ instance/frontend.tf
@@ -1,5 +1,9 @@
+data "aws_ami" "example" {
+  most_recent = true
+  owners = ["099720109477"] # Canonical
+}
 resource "aws_instance" "web" {
-  ami           = ami-0b5a2b5b8f2be4ec2
+  ami           = "ami-0b5a2b5b8f2be4ec2"
   instance_type = "t3.nano"
 
   tags = {
