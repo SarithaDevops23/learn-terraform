5391bab3f024fbca9281bb9bd839a398c7af526e
sarithadevops23@gmail.com <sarithadevops23@gmail.com>
2023-06-17 17:42:06 +0530

    issue

--- instance/frontend.tf
+++ instance/frontend.tf
@@ -1,7 +1,8 @@
-resource "aws_instance" "web" {
+resource "aws_instance" "test" {
   ami           = "ami-0b5a2b5b8f2be4ec2"
   instance_type = "t3.nano"
 
   tags = {
     Name = "first instance"
+  }
   }
