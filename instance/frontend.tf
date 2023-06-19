74c89d975faa39a367c04c46e24bb1f548390c5f
sarithadevops23@gmail.com <sarithadevops23@gmail.com>
2023-06-19 18:17:35 +0530

    a

--- instance/frontend.tf
+++ instance/frontend.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "web" {
  }
  
  data "aws_ami" "example" {
-  owners = ["099720109477"]
+  owners = ["532427859183"]
   most_recent = true
   name_regex = "Centos-8-Devops-Practice"
  }
