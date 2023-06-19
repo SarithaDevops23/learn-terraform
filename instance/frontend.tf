f88764a7a9b8e089b00b7ba04497bd5d8ac17d77
sarithadevops23@gmail.com <sarithadevops23@gmail.com>
2023-06-19 18:01:28 +0530

    2#X1Q

--- instance/frontend.tf
+++ instance/frontend.tf
@@ -1,7 +1,6 @@
 resource "aws_instance" "web" {
   ami           = data.aws_ami.example.id
-  instance_type = "t3.nano"
-  vpc_security_group_ids = [aws_security_group.sg.id]
+  instance_type = "t3.micro"
 
   tags = {
     Name = "sample"
@@ -11,23 +10,3 @@ resource "aws_instance" "web" {
   most_recent = true
   name_regex = "Centos-8-Devops-Practice"
  }
-
- resource "aws_security_group" "sg" {
-  name        = "sample"
-  description = "Allow TLS inbound traffic"
-  
-
-  ingress {
-    description      = "ssh"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
